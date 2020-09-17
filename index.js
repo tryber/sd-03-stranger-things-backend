920c63de799515faf2186bd28787c06f5b651ca3
William Igor <williamigorsilva@gmail.com>
2020-09-17 19:24:50 -0300

    alterando

--- index.js
+++ index.js
@@ -29,6 +29,4 @@ app.get('/', (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log('Escutando na seguinte porta', PORT);
-});
+app.listen(PORT, () =>  console.log('Escutando na seguinte porta', PORT));
