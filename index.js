dbf6f9c1fcea45a386500d2c52646d8a4c6496f6
William Igor <williamigorsilva@gmail.com>
2020-09-17 18:09:54 -0300

    setando variables

--- index.js
+++ index.js
@@ -29,4 +29,6 @@ app.get('/', (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => console.log('Escutando na porta', PORT));
+app.listen(PORT, () => {
+  console.log('Escutando na porta', PORT);
+});
