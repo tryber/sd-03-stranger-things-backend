ff65f19a1a9ad2c8574cd56c17cb4aa62289f47d
Jafet Henrique <jafetguerra6@gmail.com>
2020-09-17 16:16:28 -0300

    hawkins with upsidedown mode false

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 
@@ -16,17 +17,21 @@ const strangerThingsService = new StrangerThingsService(
 
 app.use(cors());
 
-const hereIsTheUpsideDown = true;
+const hereIsTheUpsideDown = () => {
+  if (process.env.UPSIDEDOWM_MODE === 'true') return true;
+  return false;
+};
 
 app.get('/', (req, res) => {
   const characters = strangerThingsService.search(
     req.query,
-    hereIsTheUpsideDown,
+    hereIsTheUpsideDown(),
   );
-
   res.status(200).json(characters);
 });
 
-app.listen(3000, () => {
-  console.log('Escutando na porta 3000');
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Escutando na porta ${PORT}`);
 });
