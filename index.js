21ed6a4ec7f6b81d35f82c6104d5572554da54eb
Rodrigo Militao <militaorodrigo9@gmail.com>
2020-09-18 17:54:14 -0300

    loggin

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const strangerThingsService = new StrangerThingsService(
 app.use(cors());
 
 const hereIsTheUpsideDown = UPSIDEDOWM_MODE === 'true';
-
+console.log(hereIsTheUpsideDown);
 app.get('/', (req, res) => {
   const characters = strangerThingsService.search(
     req.query,
