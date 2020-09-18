181cdae2e52e6b724c634d0e94bad3a8ed3a2882
Rodrigo Militao <militaorodrigo9@gmail.com>
2020-09-18 17:59:40 -0300

    last try

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 
-const { PORT, UPSIDEDOWM_MODE } = process.env;
+const { PORT = 3000, UPSIDEDOWM_MODE = true } = process.env;
 const strangerThingsDataset = require('./data/dataset/stranger-things-characters.json');
 const StrangerThingsRepository = require('./data/repository/StrangerThings');
 const StrangerThingsService = require('./services/StrangerThings');
@@ -18,8 +18,8 @@ const strangerThingsService = new StrangerThingsService(
 
 app.use(cors());
 
-const hereIsTheUpsideDown = UPSIDEDOWM_MODE === 'true';
-console.log(hereIsTheUpsideDown);
+const hereIsTheUpsideDown = UPSIDEDOWM_MODE;
+
 app.get('/', (req, res) => {
   const characters = strangerThingsService.search(
     req.query,
