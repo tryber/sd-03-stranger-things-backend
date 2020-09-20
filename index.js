0b91f1a75d190ea9f86ea3162781c1d386e6a2c2
potew <potew0@gmail.com>
2020-09-20 17:17:15 -0300

    Requisitos 1 a 5,5

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ const StrangerThingsService = require('./services/StrangerThings');
 
 const app = express();
 
+const PORT = process.env.PORT || 3000;
+const hereIsTheUpsideDown = process.env.UPSIDEDOWN_MODE || false;
+
 const strangerThingsRepository = new StrangerThingsRepository(
   strangerThingsDataset,
 );
@@ -15,8 +18,6 @@ const strangerThingsService = new StrangerThingsService(
 );
 
 app.use(cors());
-
-const hereIsTheUpsideDown = true;
 
 app.get('/', (req, res) => {
   const characters = strangerThingsService.search(
@@ -27,6 +28,6 @@ app.get('/', (req, res) => {
   res.status(200).json(characters);
 });
 
-app.listen(3000, () => {
-  console.log('Escutando na porta 3000');
+app.listen(PORT, () => {
+  console.log(`Escutando na porta ${PORT}`);
 });
