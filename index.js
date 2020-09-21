7b194eaa9b1e1f8a7b0c1e029b67236021fa3a9a
RobertoAsgon <robertoasgon@gmail.com>
2020-09-21 05:17:15 -0400

    update git user

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-require('dotenv/config');
+
 const strangerThingsDataset = require('./data/dataset/stranger-things-characters.json');
 const StrangerThingsRepository = require('./data/repository/StrangerThings');
 const StrangerThingsService = require('./services/StrangerThings');
@@ -16,18 +16,14 @@ const strangerThingsService = new StrangerThingsService(
 
 app.use(cors());
 
-const PORT = process.env.PORT;
-
-const hereIsTheUpsideDown = process.env.UPSIDEDOWN_MODE;
-
-console.log ('hereIsTheUpsideDown: ', hereIsTheUpsideDown);
+const hereIsTheUpsideDown = process.env.UPSIDEDOWN_MODE === 'true';
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   const characters = strangerThingsService.search(
     req.query,
     hereIsTheUpsideDown,
   );
-
   res.status(200).json(characters);
 });
 
