4e5c9ec5c7cb0f583cdb510dd39a4eb01f6ca760
rdgbrezolin <brezolinr@gmail.com>
2020-09-23 14:21:25 -0300

    go

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-
+require('dotenv/config');
 const strangerThingsDataset = require('./data/dataset/stranger-things-characters.json');
 const StrangerThingsRepository = require('./data/repository/StrangerThings');
 const StrangerThingsService = require('./services/StrangerThings');
@@ -17,17 +17,17 @@ const strangerThingsService = new StrangerThingsService(
 app.use(cors());
 
 const hereIsTheUpsideDown = process.env.UPSIDEDOWN_MODE === 'true';
-console.log(process.env.UPSIDEDOWN_MODE)
+console.log(typeof hereIsTheUpsideDown, hereIsTheUpsideDown);
+const { PORT } = process.env || 3000;
 
 app.get('/', (req, res) => {
   const characters = strangerThingsService.search(
     req.query,
     hereIsTheUpsideDown,
   );
+
   res.status(200).json(characters);
 });
-
-const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Escutando na porta ${PORT}`);
