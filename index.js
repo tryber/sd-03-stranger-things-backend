251ba6d768a81fd3eda77ef28ef1b8e684296a51
Rhian Castro <rhi.castro@gmail.com>
2020-10-04 14:52:28 -0300

    teste

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.get('/', (req, res) => {
 
   res.status(200).json(characters);
 });
-const PORT = process.env.PORT || 3000;
+const { PORT = 3000, UPSIDEDOWN_MODE = false } = process.env;
 app.listen(PORT, () => {
   console.log(`Escutando na porta ${PORT}`);
 });
